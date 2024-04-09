e50928d2460d0f740e0b8a7ed411c0e32be6beed
Fressia <fressiabarrios2@gmail.com>
2024-04-09 17:37:23 +0200

    created app js and app css files

---
+++ movie-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/HomePage';
+import MovieDetailPage from './pages/MovieDetailPage';
+import { InteractionContextProvider } from './context/MovieContext';
+import FavoritesPage from './pages/FavoritesPage';
+
+function App() {
+  return (
+    <div className='App'>
+      <InteractionContextProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/movie/:id' element={<MovieDetailPage/>} />
+            <Route path='/favorites' element={<FavoritesPage/>}/>
+          </Routes>
+        </Router>
+      </InteractionContextProvider>
+    </div>
+  );
+}
+
+export default App;
